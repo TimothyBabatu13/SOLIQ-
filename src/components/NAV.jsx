b980ea400a56740ea6bc360264ab95becc5a26f2
Timothy Babatunde Akanbi <knbtimothy@gmail.com>
2024-09-10 21:24:47 +0100

    Add files via upload

--- src/components/NAV.jsx
+++ src/components/NAV.jsx
@@ -1,30 +1,39 @@
+import { Link } from "react-router-dom"
 
-const NAV = () => {
+const NAV = ( { setIsOpen } ) => {
+    const handleClick = () => {
+        setIsOpen(false)
+    }
     const links = [
         {
-            href: '',
-            text: 'Trading Bot'
+            href: 'https://wa.me/+2348104131139/?text=/start',
+            text: 'Trading Bot',
+            tab: true
+        },
+        {
+            href: 'https://wa.me/+2348104131139/?text=/start',
+            text: 'Wallets Monitoring Bot',
+            tab: true
+        },
+        {
+            href: 'https://t.me/Soliqbotportal',
+            text: 'Call Channel',
+            tab: true
+        },
+        {
+            href: '#community',
+            text: 'Community',
+            tab: false,
         },
         {
             href: '',
-            text: 'Wallets Monitoring Bot'
-        },
-        {
-            href: '',
-            text: 'Call Channel'
-        },
-        {
-            href: '',
-            text: 'Community'
-        },
-        {
-            href: '',
-            text: 'Tutorials'
+            text: 'Tutorials',
+            tab: true
         }
     ]
   return (
     <nav>
-        {links.map((link, id) => <a key={id} className={id != 0 ? 'link' : ''} href={link.href}>{link.text}</a>)} 
+        {links.map((link, id) => link.tab ? <Link key={id} target="_blank" onClick={handleClick} className={id != 0 ? 'link' : ''} to={link.href}>{link.text}</Link> : <a key={id} onClick={handleClick} className={id != 0 ? 'link' : ''} href={link.href}>{link.text}</a>)} 
     </nav>
   )
 }
--- src/components/SocialHandleCard.jsx
+++ src/components/SocialHandleCard.jsx
@@ -1,14 +1,15 @@
+import { Link } from "react-router-dom"
 
 const SocialHandleCard = ({href, img, title, text }) => {
   return (
     <div className="social-card-container">
-        <a target="_blank" href={href} className="social-card">
+        <Link target="_blank" to={href} className="social-card">
             <img src={img} height={80} width={80} alt={`${img} image`} />
             <div>
                 <h5>{title}</h5>
                 <h6>{text}</h6>
             </div>
-        </a>
+        </Link>
     </div>
   )
 }
